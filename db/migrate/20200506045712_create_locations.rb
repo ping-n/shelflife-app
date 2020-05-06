5736ccaa9519789e1ba3b93fec88ffacd44d7f62
adrian-sal-kennedy <adrian.sal.kennedy@gmail.com>
2020-05-06 14:58:47 +1000

    create users, fridges, locations

---
+++ db/migrate/20200506045712_create_locations.rb
@@ -0,0 +1,11 @@
+class CreateLocations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :locations do |t|
+      t.string :name
+      t.float :volume
+      t.references :food, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
