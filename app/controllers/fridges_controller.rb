193fab745523841f58b53bb46cc424a0359b2e99
cdask <d.ask3@yahoo.com>
2020-05-07 09:49:05 +1000

    Added fridge form and new page

--- app/controllers/fridges_controller.rb
+++ app/controllers/fridges_controller.rb
@@ -1,2 +1,17 @@
 class FridgesController < ApplicationController
+  #   before_action :set_fridge, only: %i[show edit update destroy]
+  before_action :authenticate_user!
+
+  def new
+    @fridge = Fridge.new
+  end
+
+  private
+
+  def fridge_params
+    params.require(:food).permit(
+      :name,
+      :user_id
+    )
+  end
 end
