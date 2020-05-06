08d72c5eb138a97d124ace1f5f123ae0310dc55a
PING NGE <ping.nge@hotmail.com>
2020-05-06 17:17:00 +1000

    added seed file v1

---
+++ db/data/foods.rb
@@ -0,0 +1,51 @@
+# def foods
+# [
+#   {
+#     name: Faker::Food.ingredient,
+#     opened: Time.now,
+#     expiry_after_open: '2020/07/19',
+#     volume: 200,
+#     user_id: 1,
+#     location_id: 1
+#   },
+#   {
+#     name: Faker::Food.ingredient,
+#     opened: Time.now,
+#     expiry_after_open: '2020/07/19',
+#     volume: 100,
+#     user_id: 1,
+#     location_id: 2
+#   },
+#   {
+#     name: Faker::Food.ingredient,
+#     opened: Time.now,
+#     expiry_after_open: '2020/23/19',
+#     volume: 300,
+#     user_id: 2,
+#     location_id: 3
+#   },
+#   {
+#     name: Faker::Food.ingredient,
+#     opened: Time.now,
+#     expiry_after_open: '2020/10/25',
+#     volume: 30,
+#     user_id: 2,
+#     location_id: 3
+#   },
+#   {
+#     name: Faker::Food.ingredient,
+#     opened: Time.now,
+#     expiry: '2020/08/23',
+#     volume: 100,
+#     user_id: 3,
+#     location_id: 3
+#   },
+#   {
+#     name: Faker::Food.ingredient,
+#     expiry: '2020/07/19',
+#     volume: 200
+#     user_id: 3,
+#     location_id: 2
+#   }
+# ]
+# end
