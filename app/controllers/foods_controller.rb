8cc38ccac1839c9200718df1cf154df569479692
cdask <d.ask3@yahoo.com>
2020-05-06 17:26:26 +1000

    Added CRUD controller functionality to food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,14 +1,53 @@
 class FoodsController < ApplicationController
+  before_action :set_food, only: %i[show update destroy]
+
   def index
-    
+    @foods = Food.where(user: current_user)
   end
 
-  def show
+  def show; end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = current_user.foods.create(food_params)
+    if @food.errors.any?
+      render :new
+    else
+      flash[:success] = 'You successfully added a food!'
+      redirect_to @food
+    end
+  end
+
+  def update
+    if @food.update(food_params)
+      redirect_to @food
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @food.destroy
   end
 
   private
+
   def food_params
-    params.require(:foods).permit(:name, :opened, :expiry, :expiry_after_open, :volume, :user_id, :location_id)
+    params.require(:foods).permit(
+      :name, 
+      :opened, 
+      :expiry, 
+      :expiry_after_open, 
+      :volume, 
+      :user_id, 
+      :location_id
+    )
+  end
+
+  def set_food
+    @food = Food.find(params[:id])
   end
 end
-
