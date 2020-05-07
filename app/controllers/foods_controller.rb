0a1e8eec86c7bd6b67a5a0a4ea2932656ddf0a21
adrian-sal-kennedy <adrian.sal.kennedy@gmail.com>
2020-05-08 00:43:34 +1000

    chasing down errors on edit/create

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,6 @@
 class FoodsController < ApplicationController
   before_action :set_food, only: %i[show edit update destroy]
+  # before_action :set_location
   before_action :authenticate_user!
   load_and_authorize_resource
 
@@ -12,19 +13,28 @@ class FoodsController < ApplicationController
 
   def new
     @food = Food.new
-    # @fridge = current_user.fridges.find(:fridge_id)
+    @fridge = current_user.fridges.find(params[:fridge_id])
+  end
+  def new_in_fridge
+    @food = Food.new
     @fridge = current_user.fridges.find(params[:fridge_id])
   end
 
-  def edit; end
+  def edit
+    @fridge = current_user.fridges.find(params[:fridge_id])
+  end
+  def edit_in_fridge
+    @fridge = current_user.fridges.find(params[:fridge_id])
+  end
 
   def create
-    @food = current_user.foods.create(food_params)
+    @food = current_user.foods.create!(food_params)
     if @food.errors.any?
-      render :new
+      # render :new]
+      redirect_to root_path
     else
-      flash[:success] = 'You successfully added a food!'
-      redirect_to @food
+      # flash[:success] = 'You successfully added a food!'
+      redirect_to root_path
     end
   end
 
