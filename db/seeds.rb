a4fa5ee58d38cbd97d28eee81a36573cee538d3b
PING NGE <ping.nge@hotmail.com>
2020-05-07 12:49:27 +1000

    updated seed file with changes to make it more readable

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,46 @@
 require_relative 'data/foods'
+require_relative 'data/locations'
 
 puts 'Seeding Users'
-
+p "--" * 17
 user = User.create(email: 'daniel@gmail.com', password: 'password')
+puts "Created User 1"
 user2 = User.create(email: 'adrian@gmail.com', password: 'password')
+puts "Created User 2"
 user3 = User.create(email: 'ping@gmail.com', password: 'password')
-
+puts "Created User 3"
+p "--" * 17
 puts 'Seeded Users'
+p "--" * 17
 
 puts 'Seeding Fridges'
-
+p "--" * 17
 fridge = Fridge.create(name: "Daniel's Fridge", user_id: 1)
+puts "Created #{fridge[:name]}"
 fridge_2 = Fridge.create(name: "Adrian's Fridge", user_id: 2)
-
+puts "Created #{fridge_2[:name]}"
+fridge_3 = Fridge.create(name: "Ping's Fridge", user_id: 3)
+puts "Created #{fridge_3[:name]}"
+p "--" * 17
 puts 'Seeded Fridges'
+p "--" * 17
 
 puts 'Seeding locations'
-
-top_shelf = Location.create(name: 'Top Shelf', volume: 1000, fridge_id: 1)
-bottom_shelf = Location.create(name: 'Bottom Shelf', volume: 1000, fridge_id: 1)
-freeze = Location.create(name: 'Freezer', volume: 1000, fridge_id: 1)
-
-top_shelf_2 = Location.create(name: 'Top Shelf', volume: 1000, fridge_id: 2)
-bottom_shelf_2 = Location.create(name: 'Bottom Shelf', volume: 1000, fridge_id: 2)
-freeze_2 = Location.create(name: 'Freezer', volume: 1000, fridge_id: 2)
-
+p "--" * 17
+locations.each do |location|
+  location = Location.create(location)
+  puts "Created #{location[:name]}"
+end
+p "--" * 17
 puts 'Seeded Locations'
-
+p "--" * 17
 puts 'Seeding Foods'
-
+p "--" * 17
 foods.each do |food|
   food = Food.create(food)
   puts "Created #{food[:name]}"
 end
+p "--" * 17
 puts 'Seeded Foods'
+p "--" * 17
+puts "Seeding was successful!"
