f77d7438934268aa22cc13c98f6a1ed5d47d4021
cdask <d.ask3@yahoo.com>
2020-05-07 14:33:36 +1000

    Added Delete functionality to locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,19 +1,30 @@
 class LocationsController < ApplicationController
-    before_action :authenticate_user!
+  before_action :set_fridge
+  before_action :authenticate_user!
 
-    def create
-        @fridge = Fridge.find(params[:fridge_id])
-        @location = @fridge.location.create(location_params)
-        if @location.errors.any?
-            render :new
-        else
-            flash[:success] = 'Added fridge location'
-            redirect_to fridge_path(@fridge)
-        end
+  def create
+    @location = @fridge.location.create(location_params)
+    if @location.errors.any?
+      render :new
+    else
+      flash[:success] = 'Added fridge location'
+      redirect_to fridge_path(@fridge)
     end
+  end
 
-    private
-    def location_params
-        params.require(:location).permit(:name,:volume)
-    end
+  def destroy
+    @location = @fridge.location.find(params[:id])
+    @location.destroy
+    redirect_to @fridge
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:name, :volume)
+  end
+
+  def set_fridge
+    @fridge = Fridge.find(params[:fridge_id])
+  end
 end
