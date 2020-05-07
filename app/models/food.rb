69769d3b4abfbfbeefd4ae8c8102114999e7d770
PING NGE <ping.nge@hotmail.com>
2020-05-07 11:20:25 +1000

    added validation to food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,7 +2,7 @@ class Food < ApplicationRecord
   belongs_to :location
   belongs_to :user
   has_one_attached :picture
-
+  validates :name, :expiry, presence: true
 
   def expired?
     expired = Date.today < self.expiry
