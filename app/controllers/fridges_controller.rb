0870406b996f97a970e3e8343d30fda44c56d648
cdask <d.ask3@yahoo.com>
2020-05-07 10:31:50 +1000

    Added Update for fridge

--- app/controllers/fridges_controller.rb
+++ app/controllers/fridges_controller.rb
@@ -1,5 +1,5 @@
 class FridgesController < ApplicationController
-  before_action :set_fridge, only: %i[show]
+  before_action :set_fridge, only: %i[show edit update]
   before_action :authenticate_user!
 
   def show; end
@@ -8,6 +8,8 @@ class FridgesController < ApplicationController
     @fridge = Fridge.new
   end
 
+  def edit; end
+
   def create
     @fridge = current_user.fridges.create(fridge_params)
     if @fridge.errors.any?
@@ -15,6 +17,14 @@ class FridgesController < ApplicationController
     else
       flash[:success] = 'You successfully added a fridge'
       redirect_to root_path
+    end
+  end
+
+  def update
+    if @fridge.update(fridge_params)
+        redirect_to @fridge
+    else
+        render :edit
     end
   end
 
