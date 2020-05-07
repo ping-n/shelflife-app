49f406aeafbb0c02f5c1dd99853dd5cebf41d6e6
PING NGE <ping.nge@hotmail.com>
2020-05-07 11:05:19 +1000

    adde expired? method to model

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,12 @@ class Food < ApplicationRecord
   belongs_to :location
   belongs_to :user
   has_one_attached :picture
+
+
+  def expired?
+    expired = Date.today < self.expiry
+    if expired == false
+      return "#{self.name} have expired"
+    end
+  end
 end
