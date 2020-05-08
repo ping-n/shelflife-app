2aa6143014f42e416d969cdd18f0e9185712b441
cdask <d.ask3@yahoo.com>
2020-05-08 20:08:36 +1000

    Fixed link bugs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   
   resources :foods
   devise_for :users
-  get "/fridges", to: "fridges#index"
   resources :fridges do
     resources :locations
     resources :foods
